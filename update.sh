1e4552029e0dd2012cf65aacb0c1f64352f2e661
Jan Werner <jwerner@microtech.de>
2020-11-07 16:04:19 +0100

    Guide data update 2020-11-07 16:04

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env bash
 cp /opt/easyepg/xml/guide.xml /opt/tv/guide.xml
 git add --all
-git commit -m "Guide data update $(date +'%Y-%m-%d')"
+git commit -m "Guide data update $(stat --format=%y /opt/tv/guide.xml | head -c 16)"
 git push
